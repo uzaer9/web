9bd2559896f504b8a4dcc92ca5a8c5b21d51b69d
uzaer9 <108221436+uzaer9@users.noreply.github.com>
2026-02-14 02:40:43 +0530

    Remove

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react/jsx-runtime'],
+//     },
+//   },
+// });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react/jsx-runtime'],
-    },
-  },
-});
+  base: '/',
+})
